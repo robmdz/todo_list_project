91c66e16d36604d228b19eeb82dc8cd20d1e20e2
robmdz <rmendozac@unal.edu.co>
2026-04-22 23:45:04 -0500

    I solved an issue with the delete button.

--- code.js
+++ code.js
@@ -1,29 +1,26 @@
 function add_tasks(info){
-    task = document.createElement("label")
-    task_checkbox = document.createElement("input")
-    task_checkbox.type = "checkbox"
-    task_information = document.createTextNode(info)
-    delete_button = document.createElement("button")
+    const task_list = document.getElementById("tasks_list")
+    const task_element = task_list.appendChild(document.createElement("li"))
+    task_element.appendChild(document.createTextNode(info))
+    const delete_button = task_element.appendChild(document.createElement("button"))
     delete_button.style.backgroundColor = "red"
     delete_button.style.padding = "8px"
-    document.body.appendChild(task_checkbox)
-    document.body.appendChild(task_information)
-    document.body.appendChild(delete_button)
-    delete_button.addEventListener("click", ()=>{
-        delete_task()
-})
+    delete_button.addEventListener("click", (e)=> {
+        delete_task(e.target.parentElement)})
 }
 
 const input_information = document.getElementById("new_task")
-const btn = document.getElementById("add_task");
+const btn = document.getElementById("add_task")
+
 btn.addEventListener("click", ()=>{
-    add_tasks(input_information.value)
+    if (input_information.value != ""){
+        add_tasks(input_information.value)
+    }
     input_information.value = ""
 })
 
 
-function delete_task(){
-    task_checkbox.remove();
-    task_information.remove();
-    delete_button.remove()
+function delete_task(task){
+    task.remove()
+
 }
