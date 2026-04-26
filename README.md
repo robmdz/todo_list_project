f98abe7354b61b4d727a5936b8de82b571a54d60
robmdz <rmendozac@unal.edu.co>
2026-04-26 11:37:40 -0500

    I added a mockup, solved the button alignment problem, and changed the title position.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Todo List Project
 This project was build to learn Fundamental skills (HTML, CSS and JS).
 
+![mockup ](/icons%20and%20imgs/Home.png)
+
 ### Color palette:
 #f22c40
 
--- code.js
+++ code.js
@@ -3,17 +3,21 @@ function add_tasks(info){
 
     const task_element = task_list.appendChild(document.createElement("li"))
     task_element.id = "task_element"
-    task_element.appendChild(document.createTextNode(info))
-    task_element_buttons = task_element.appendChild(document.createElement("div"))
-    task_element_buttons.id = "task_element_buttons"
 
-    const delete_icon = task_element_buttons.appendChild(document.createElement("img"))
-    delete_icon.src = "/icons and imgs/delete_button_img.svg"
-    delete_icon.id = "delete_icon"
+    const task_container = task_element.appendChild(document.createElement("div"))
+    task_container.id = "task_container"
 
-    const check_icon = task_element_buttons.appendChild(document.createElement("img"))
+    const check_icon = task_container.appendChild(document.createElement("img"))
     check_icon.src = "/icons and imgs/check_button.svg"
     check_icon.id = "check_icon"
+
+    const task_text = task_container.appendChild(document.createElement("span"))
+    task_text.appendChild(document.createTextNode(info))
+    task_text.id = "task_text"
+
+    const delete_icon = task_container.appendChild(document.createElement("img"))
+    delete_icon.src = "/icons and imgs/delete_button_img.svg"
+    delete_icon.id = "delete_icon"
 
     let isClicked = false
     check_icon.addEventListener("click", ()=> {
@@ -22,7 +26,7 @@ function add_tasks(info){
         })
 
     delete_icon.addEventListener("click", (e)=> {
-        delete_task(e.target.parentElement.parentElement)}) 
+        delete_task(e.target.parentElement)}) 
 }
 
 const input_information = document.getElementById("new_task")
